b8f942f2fdf0a7215f6f8c4979adcf308ce774e4
Billouboq <aurelien.boquet.web@gmail.com>
2017-01-11 23:48:59 +0100

    move socket to a controller file

--- sockets/index.js
+++ sockets/index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+module.exports = function (io) {
+
+   io.on('connection', (socket) => {
+      console.log('connected');
+
+      socket.on('sendVideo', (id) => {
+         io.emit('video', id);
+      });
+
+      socket.on('sendPlay', () => {
+         io.emit('play');
+      });
+
+      socket.on('sendPause', () => {
+         io.emit('pause');
+      });
+
+      socket.on('sendStop', () => {
+         io.emit('stop');
+      });
+
+   });
+
+}
